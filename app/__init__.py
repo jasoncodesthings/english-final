636f40a65be3a70ea5ad0689b95f3b602eb66b75
Jason Chan <jchan61@stuy.edu>
2026-06-05 00:00:47 -0400

    oh god im def not gonna sleep today

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,31 @@ from flask import redirect, url_for
 import sqlite3   #enable control of an sqlite database
 import datetime
 
+import urllib.request
+import urllib.error
+import json
+import time
+import random
+
+app = Flask(__name__)
+app.secret_key = "secret"
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return redirect(url_for("home"))
+
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route("/cs", methods=['GET', 'POST'])
+def cs():
+    return render_template('cs.html')
+
+@app.route("/chorus", methods=['GET', 'POST'])
+def chorus():
+    return render_template('chorus.html')
+
 
 if __name__=='__main__':
     app.debug = True
